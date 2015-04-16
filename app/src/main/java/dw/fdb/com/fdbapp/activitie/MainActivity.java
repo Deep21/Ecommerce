8bb357a3bd382d210dab5e6c8d34b4b217da2467
Sam Fisher <samfisher8456@hotmail.com>
2015-04-16 21:16:43 +0200

    change ws address

--- app/src/main/java/dw/fdb/com/fdbapp/activitie/MainActivity.java
+++ app/src/main/java/dw/fdb/com/fdbapp/activitie/MainActivity.java
@@ -66,7 +66,6 @@ public class MainActivity extends BaseAbstractActivity implements CategoryListFr
         //addOrderListFragment();
         //addHomeCustomerFragment();
         addConnexionFragment();
-
         //addCategoryFragment();
         //addProductListFragment();
         //addProductDetailFragment();
