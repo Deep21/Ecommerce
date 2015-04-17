d43bf2563c453e288fd3f4c92df4445c66c489c3
Sam Fisher <samfisher8456@hotmail.com>
2015-04-17 02:34:03 +0200

    integrate betterpicker

--- app/src/main/java/dw/fdb/com/fdbapp/fragments/CustomerCreateFragment.java
+++ app/src/main/java/dw/fdb/com/fdbapp/fragments/CustomerCreateFragment.java
@@ -2,6 +2,7 @@ package dw.fdb.com.fdbapp.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,16 +12,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.doomonafireball.betterpickers.calendardatepicker.CalendarDatePickerDialog;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
+
+import org.joda.time.DateTime;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import dw.fdb.com.fdbapp.R;
 import dw.fdb.com.fdbapp.model.Customer;
-import dw.fdb.com.fdbapp.request.CustomerCreateRequest;
 
-public class CustomerCreateFragment extends BaseFragment {
+public class CustomerCreateFragment extends BaseFragment implements CalendarDatePickerDialog.OnDateSetListener{
 
     public static final String TAG = "CustomerCreateFragment";
     @InjectView(R.id.nom)
@@ -35,6 +38,7 @@ public class CustomerCreateFragment extends BaseFragment {
     TextView pwd_confirm;
     View view;
     FragmentListner fragmentListner;
+    private static final String FRAG_TAG_DATE_PICKER = "fragment_date_picker_name";
 
     public static CustomerCreateFragment getInstance() {
         CustomerCreateFragment connexionFragment = new CustomerCreateFragment();
@@ -100,8 +104,20 @@ public class CustomerCreateFragment extends BaseFragment {
     }
 
     private void perform_request() {
-        CustomerCreateRequest customerCreateRequest = new CustomerCreateRequest(getCustomer());
-        getSpiceManager().execute(customerCreateRequest, new CreateCustomerListner());
+        FragmentManager fm = getChildFragmentManager();
+        DateTime now = DateTime.now();
+        CalendarDatePickerDialog calendarDatePickerDialog = CalendarDatePickerDialog
+                .newInstance(CustomerCreateFragment.this, now.getYear(), now.getMonthOfYear() - 1,
+                        now.getDayOfMonth());
+        calendarDatePickerDialog.show(fm, FRAG_TAG_DATE_PICKER);
+
+//        CustomerCreateRequest customerCreateRequest = new CustomerCreateRequest(getCustomer());
+//        getSpiceManager().execute(customerCreateRequest, new CreateCustomerListner());
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialog calendarDatePickerDialog, int i, int i2, int i3) {
+
     }
 
     public class CreateCustomerListner implements RequestListener<Customer> {
@@ -109,11 +125,11 @@ public class CustomerCreateFragment extends BaseFragment {
         @Override
         public void onRequestFailure(SpiceException e) {
 
-
         }
 
         @Override
         public void onRequestSuccess(Customer customer) {
+
             Log.e("CustomerCreateFragment", "" + customer);
 //            if (customer.getCode() == 201) {
 //                System.out.println("ok");
